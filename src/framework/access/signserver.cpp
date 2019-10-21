fe2279a3923ee11922ecf8bc1672d71e3c21d968
Dmitry Baryshnikov <bishop.dev@gmail.com>
2019-10-22 01:42:24 +0300

    Fix includes

--- src/framework/access/signserver.cpp
+++ src/framework/access/signserver.cpp
@@ -18,6 +18,7 @@
 *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ******************************************************************************/
 #include "signserver.h"
+#include "access.h"
 
 #include <QCoreApplication>
 #include <QDesktopServices>
