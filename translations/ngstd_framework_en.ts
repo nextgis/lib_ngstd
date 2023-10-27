6ebf47b5cf6c26dc8354f0c93fa6b1037d682e03
a.tolstoy <sharukan81v@gmail.com>
2023-10-27 08:42:47 +0300

    add translate #8692v7rg6

--- translations/ngstd_framework_en.ts
+++ translations/ngstd_framework_en.ts
@@ -4,28 +4,29 @@
 <context>
     <name>NGAccess</name>
     <message>
-        <location filename="../src/framework/access/access.cpp" line="90"/>
+        <location filename="../src/framework/access/access.cpp" line="112"/>
         <source>&lt;a href=&quot;https://nextgis.com/pricing-base/&quot;&gt;Learn more ...&lt;/a&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/framework/access/access.cpp" line="93"/>
-        <location filename="../src/framework/access/access.cpp" line="98"/>
+        <location filename="../src/framework/access/access.cpp" line="115"/>
+        <location filename="../src/framework/access/access.cpp" line="120"/>
         <source>Unsupported</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/framework/access/access.cpp" line="94"/>
+        <location filename="../src/framework/access/access.cpp" line="116"/>
         <source>Please upgrade and sign in to use this feature.&lt;br&gt;%1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/framework/access/access.cpp" line="99"/>
+        <location filename="../src/framework/access/access.cpp" line="121"/>
         <source>Please upgrade to use this feature.&lt;br&gt;%1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/framework/access/access.cpp" line="316"/>
+        <location filename="../src/framework/access/access.cpp" line="584"/>
+        <location filename="../src/framework/access/access.cpp" line="591"/>
         <source>Failed open file %1</source>
         <translation type="unfinished"></translation>
     </message>
@@ -46,57 +47,57 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/framework/mainwindow.cpp" line="44"/>
+        <location filename="../src/framework/mainwindow.cpp" line="43"/>
         <source>File</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/framework/mainwindow.cpp" line="45"/>
+        <location filename="../src/framework/mainwindow.cpp" line="44"/>
         <source>&amp;File</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/framework/mainwindow.cpp" line="46"/>
+        <location filename="../src/framework/mainwindow.cpp" line="45"/>
         <source>&amp;Help</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/framework/mainwindow.cpp" line="135"/>
+        <location filename="../src/framework/mainwindow.cpp" line="134"/>
         <source>&amp;Open</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/framework/mainwindow.cpp" line="137"/>
+        <location filename="../src/framework/mainwindow.cpp" line="136"/>
         <source>Open project file</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/framework/mainwindow.cpp" line="142"/>
+        <location filename="../src/framework/mainwindow.cpp" line="141"/>
         <source>&amp;Quit</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/framework/mainwindow.cpp" line="144"/>
+        <location filename="../src/framework/mainwindow.cpp" line="143"/>
         <source>Quit program</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/framework/mainwindow.cpp" line="151"/>
+        <location filename="../src/framework/mainwindow.cpp" line="150"/>
         <source>About</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/framework/mainwindow.cpp" line="153"/>
+        <location filename="../src/framework/mainwindow.cpp" line="152"/>
         <source>About program</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/framework/mainwindow.cpp" line="159"/>
+        <location filename="../src/framework/mainwindow.cpp" line="158"/>
         <source>&amp;Preferences</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/framework/mainwindow.cpp" line="161"/>
+        <location filename="../src/framework/mainwindow.cpp" line="160"/>
         <source>Application preferences</source>
         <translation type="unfinished"></translation>
     </message>
@@ -118,58 +119,75 @@
     <name>NGSignDialog</name>
     <message>
         <location filename="../src/framework/access/signdialog.ui" line="14"/>
+        <location filename="../build/src/framework/ngstd_framework_autogen/include/ui_signdialog.h" line="137"/>
         <source>Dialog</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/framework/access/signdialog.ui" line="26"/>
+        <location filename="../src/framework/access/signdialog.ui" line="111"/>
+        <location filename="../build/src/framework/ngstd_framework_autogen/include/ui_signdialog.h" line="143"/>
+        <source>We&apos;re unable to connect to authorization server. Make sure your connection settings are correct under:
+ - Settings-&gt;Options-&gt;NextGIS
+ - Settings-&gt;Options-&gt;Network
+or contact your system administrator.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/framework/access/signdialog.ui" line="142"/>
+        <location filename="../build/src/framework/ngstd_framework_autogen/include/ui_signdialog.h" line="147"/>
         <source>Sign in/out</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/framework/access/signdialog.ui" line="52"/>
+        <location filename="../src/framework/access/signdialog.ui" line="26"/>
+        <location filename="../build/src/framework/ngstd_framework_autogen/include/ui_signdialog.h" line="138"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;img src=&quot;:/images/ng_logo_bw_dark.svg&quot; height=&quot;20&quot;/&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/framework/access/signdialog.ui" line="68"/>
+        <location filename="../src/framework/access/signdialog.ui" line="72"/>
+        <location filename="../build/src/framework/ngstd_framework_autogen/include/ui_signdialog.h" line="141"/>
         <source>Sign in with your NextGIS account to get extended capabilities</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/framework/access/signdialog.ui" line="87"/>
-        <location filename="../src/framework/access/signdialog.cpp" line="42"/>
+        <location filename="../src/framework/access/signdialog.ui" line="91"/>
+        <location filename="../build/src/framework/ngstd_framework_autogen/include/ui_signdialog.h" line="142"/>
+        <location filename="../src/framework/access/signdialog.cpp" line="85"/>
         <source>&lt;a href=&quot;https://nextgis.com/pricing-base/&quot;&gt;Learn more ...&lt;/a&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/framework/access/signdialog.ui" line="103"/>
-        <location filename="../src/framework/access/signdialog.ui" line="116"/>
+        <location filename="../src/framework/access/signdialog.ui" line="44"/>
+        <location filename="../src/framework/access/signdialog.ui" line="57"/>
+        <location filename="../build/src/framework/ngstd_framework_autogen/include/ui_signdialog.h" line="139"/>
+        <location filename="../build/src/framework/ngstd_framework_autogen/include/ui_signdialog.h" line="140"/>
         <source>TextLabel</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/framework/access/signdialog.cpp" line="24"/>
-        <source>Exit</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../src/framework/access/signdialog.cpp" line="25"/>
+        <location filename="../src/framework/access/signdialog.cpp" line="66"/>
         <source>Account</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/framework/access/signdialog.cpp" line="31"/>
+        <location filename="../src/framework/access/signdialog.cpp" line="45"/>
+        <location filename="../src/framework/access/signdialog.cpp" line="68"/>
         <source>Supported</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/framework/access/signdialog.cpp" line="31"/>
+        <location filename="../src/framework/access/signdialog.cpp" line="61"/>
+        <source>Sign out</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/framework/access/signdialog.cpp" line="68"/>
         <source>Unsupported</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/framework/access/signdialog.cpp" line="41"/>
+        <location filename="../src/framework/access/signdialog.cpp" line="84"/>
         <source>Sign in</source>
         <translation type="unfinished"></translation>
     </message>
@@ -177,23 +195,33 @@
 <context>
     <name>NGSignServer</name>
     <message>
-        <location filename="../src/framework/access/signserver.cpp" line="58"/>
+        <location filename="../src/framework/access/signserver.cpp" line="107"/>
         <source>Please sign in
 via the opened browser...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/framework/access/signserver.cpp" line="63"/>
+        <location filename="../src/framework/access/signserver.cpp" line="199"/>
         <source>You have successfully signed in &lt;br&gt; at &lt;span&gt;%1&lt;/span&gt; application</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/framework/access/signserver.cpp" line="66"/>
+        <location filename="../src/framework/access/signserver.cpp" line="203"/>
+        <source>Failed to signed in &lt;br&gt; at &lt;span&gt;%1&lt;/span&gt; application.&lt;br&gt;Error: %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/framework/access/signserver.cpp" line="208"/>
         <source>Successfully signed</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/framework/access/signserver.cpp" line="68"/>
+        <location filename="../src/framework/access/signserver.cpp" line="208"/>
+        <source>Failed to signed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/framework/access/signserver.cpp" line="211"/>
         <source>Now you can close this page</source>
         <translation type="unfinished"></translation>
     </message>
